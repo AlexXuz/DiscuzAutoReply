872571c7e90c6a67592ca72ee63c417fe5027045
xuz <mq5O16kADCLvHeJy>
2019-12-17 13:33:31 +0800

    提交代码

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
@@ -106,16 +105,3 @@ public class Main {
     }
 }
 
-class StreamTool {
-    public static byte[] read(InputStream inputStr) throws Exception {
-        ByteArrayOutputStream outStr = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int len = 0;
-        while ((len = inputStr.read(buffer)) != -1) {
-            outStr.write(buffer, 0, len);
-        }
-        inputStr.close();
-        return outStr.toByteArray();
-    }
-
-}
